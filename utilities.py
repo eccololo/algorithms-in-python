f388f2f598ef8e17ed36299b6c43a4b0690ee18e
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2021-02-19 17:46:08 +0100

    Creating body of function that generate random list in utilities.py

--- utilities.py
+++ utilities.py
@@ -1,2 +1,10 @@
-def create_random_shuffles_list(max, min):
-    pass
+import random
+
+
+def create_random_shuffles_list(max_list, min_list, max_item, min_item):
+    random_list = []
+    for n in range(min_list, max_list):
+        number = random.randint(min_item, max_item)
+        random_list.append(number)
+
+    return random_list
