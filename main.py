90f27b6396e301f6f07376abe2d233afd9a6ad4d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2021-02-19 17:45:28 +0100

    Generating random list from imported utilities.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import brutal_force_search
+import utilities
 import random
 
-min = 1
-max = 10000
+min_list_range = 1
+max_list_range = 10000
+min_item = 1
+max_item = 101
 searched_number = 101
-random_list = []
-for n in range(min, max):
-    number = random.randint(1, 100000)
-    random_list.append(number)
-    
+
+utilities.create_random_shuffles_list(max_list_range, min_list_range, max_item, min_item)
+
 number_of_searches = brutal_force_search.brutal_search(random_list, searched_number)
 print(f"Brutal search searched {searched_number} number {number_of_searches} times.")
