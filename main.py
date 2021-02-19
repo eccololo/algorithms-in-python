d07fbefdba32e447fc7d1c125a425a9637742944
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2021-02-19 17:47:22 +0100

    Minor changes in main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import brutal_force_search
 import utilities
-import random
 
 min_list_range = 1
 max_list_range = 10000
-min_item = 1
-max_item = 101
+min_item_range = 1
+max_item_range = 101
 searched_number = 101
 
-utilities.create_random_shuffles_list(max_list_range, min_list_range, max_item, min_item)
+random_list = utilities.create_random_shuffles_list(max_list_range, min_list_range, max_item_range, min_item_range)
 
 number_of_searches = brutal_force_search.brutal_search(random_list, searched_number)
 print(f"Brutal search searched {searched_number} number {number_of_searches} times.")
