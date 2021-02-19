60b7451d515a907861a6d90c42e1ece9a3238d10
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2021-02-19 17:39:11 +0100

    Creating testing brutal search code in main.py.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import brutal_force_search
+import random
 
+min = 1
+max = 10000
+searched_number = 101
+random_list = []
+for n in range(min, max):
+    number = random.randint(1, 100000)
+    random_list.append(number)
+    
+number_of_searches = brutal_force_search.brutal_search(random_list, searched_number)
+print(f"Brutal search searched {searched_number} number {number_of_searches} times.")
