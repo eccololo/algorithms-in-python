d10dd7dae5e922cdd76f0919b68eba8d90dc05eb
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2021-03-01 10:04:19 +0100

    Adding comments in brutal_force_search.py.

--- brutal_force_search.py
+++ brutal_force_search.py
@@ -1,3 +1,4 @@
+# This is brutal force search function. Search for an element from begining to an end.
 def brutal_search(myList, target):
     for i in range(len(myList)):
         if myList[i] == target:
