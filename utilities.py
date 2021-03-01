8080ab2e2f0beb9bebdfcd5bb1ee6d8d929ec1e1
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2021-03-01 10:03:02 +0100

    Adding comments to functions in utilities.py

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import random
 
 
+# Creates random shuffled list from min to max elements number with min to max elements
 def create_random_shuffles_list(max_list, min_list, max_item, min_item):
     random_list = []
     for n in range(min_list, max_list):
@@ -10,5 +11,7 @@ def create_random_shuffles_list(max_list, min_list, max_item, min_item):
     return random_list
 
 
+# Print execution time of some intruction or function.
 def print_execution_time(start, stop, text):
+    text += " execution time:"
     print(text, "{0:.8f}s,".format(stop - start))
