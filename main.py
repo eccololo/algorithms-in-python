65119b66a8725bc136f0184e3e453f5f80639258
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2021-03-01 10:25:54 +0100

    Adding main loop in main.py.

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-import brutal_force_search
+import brutal_force
 import utilities
 import time
 import math
+
+utilities.print_welcome()
+
+# Main Loop
+while True:
+    utilities.print_main_menu()
+    choice = input("What is your choice?: ")
+    if choice.lower() == "q":
+        print("Goodbye.")
+        break
+    elif choice.lower() == "w":
+        utilities.print_welcome()
+
+    if choice == 1:
+        print("**********************")
+        print("* Search Algorithms: ")
+    elif choice == 2:
+        print("**********************")
+        print("* Sorting Algorithms: ")
+    elif choice == 3:
+        print("**********************")
+        print("* Brutal Force Algorithms: ")
+    elif choice == 4:
+        print("**********************")
+        print("* Computational Complexity of Algorithms: ")
 
 min_list_range = 1
 max_list_range = 10000
@@ -14,7 +39,7 @@ random_list = utilities.create_random_shuffles_list(max_list_range, min_list_ran
 stop_list_creation = time.time()
 
 start_bs = time.time()
-number_of_searches = brutal_force_search.brutal_search(random_list, searched_number)
+number_of_searches = brutal_force.brutal_search(random_list, searched_number)
 stop_bs = time.time()
 
 print(f"Brutal search searched {searched_number} number {number_of_searches} times in shuffled list.")
